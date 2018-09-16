e5902bb3a180dd78839ab284f17b1463fc5c4466
andrea <ap3rg@virginia.edu>
2018-09-15 19:32:04 -0500

    mapper sin necesidad de acceder al archivo taxi_zone_lookup

--- MRjob/RF1/MR_RF1.py
+++ MRjob/RF1/MR_RF1.py
@@ -4,6 +4,50 @@ from mrjob.step import MRStep
 
 class MRMostUsedWord(MRJob):
 
+    #supporting methods
+    def mapper_init():
+        '''
+        This method generates a dictionary from the file taxi_zone_lookup.csv
+        '''
+
+        # Absolute path, script is in
+        script_dir = os.path.dirname(__file__)
+        # relative path
+        rel_path = 'LocationByID/taxi_zone_lookup.csv'
+        abs_file_path = os.path.join(script_dir, rel_path)
+
+        # get data to format index to use in location by data method
+        f = open(abs_file_path, 'r')
+        data = f.read()
+        data = data.split('\n')
+
+        zones = []
+        location_by_ID = {}
+
+        for line in data:
+            line = line.strip()
+            # print(line)
+            location, zone, trash = line.split(',', 2)
+
+            if zone not in zones:
+                location_by_ID[zone] = []
+
+        for line in data:
+            line = line.strip()
+            location, zone, trash = line.split(',', 2)
+
+            if (location not in location_by_ID[zone]):
+                location_by_ID[zone].append(location)
+
+        return location_by_ID
+
+        # Create the location dictionary
+
+    location_dictionary = location_by_ID()
+
+
+
+
     def steps(self):
         return [
             MRStep(mapper=self.mapper_get_words,
@@ -13,6 +57,23 @@ class MRMostUsedWord(MRJob):
         ]
 
     def mapper_get_words(self, _, line):
+
+        # input comes from STDIN (standard input)
+        # Input enters in a CSV scheme
+        # Each line is a record
+
+        line = line.strip()
+        data = line.split(',')
+
+        destinationID = get_value('END_LOC', data)
+        destination = location_by_ID_lookup(location_dictionary, destinationID)
+
+        pickupTime = get_value('START_DATE', data)
+        pickupTime = datetimeToInt(pickupTime)
+        if destination:
+            if inTimeRange(start, end, pickupTime):
+                print('%s\t%s' % (destination, 1))
+
         # yield each word in the line
         for word in WORD_RE.findall(line):
             yield (word.lower(), 1)
